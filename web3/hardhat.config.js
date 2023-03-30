71ed0f8b3e9ac5348b95902a05495fbdd3f9cf9a
Keith Hall Barnett <thecrowbill@yahoo.com>
2023-03-30 15:48:12 -0500

    Add deploy params for Mordor

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   solidity: {
     version: '0.8.9',
+    defaultNetwork: 'mordor',
+    networks: {
+      hardhat: {},
+      mordor: {
+        url: 'https://geth-mordor.etc-network.info',
+        accounts: {'0x$(process.env.PRIVATE_KEY)'}
+      }
+    }
     settings: {
       optimizer: {
         enabled: true,
