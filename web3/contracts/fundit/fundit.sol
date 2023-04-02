87f90358db9155e2e0643f362be38452c1351c9b
Keith Hall Barnett <thecrowbill@yahoo.com>
2023-04-02 04:52:16 -0500

    Pragma solidity compiler version 0.8.9 set

--- web3/contracts/fundit/fundit.sol
+++ web3/contracts/fundit/fundit.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
