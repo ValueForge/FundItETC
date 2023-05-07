80d6219585836f39187cdf94aa25a12200db4a35
Keith Hall Barnett <thecrowbill@yahoo.com>
2023-05-07 16:15:56 -0500

    Update fundit.test.js script

--- web3/test/fundIt.test.js
+++ web3/test/fundIt.test.js
@@ -1,6 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const { BigNumber } = ethers;
+const { ProxyAdmin } = require('@openzeppelin/contracts/proxy/transparent/ProxyAdmin');
 
 describe("FundIt", function () {
   let FundItFactory, fundIt, FundItStorageFactory, fundItStorage, FundItProxyFactory, fundItProxy;
@@ -8,7 +9,7 @@ describe("FundIt", function () {
   const TITLE = "Test Campaign";
   const DESCRIPTION = "This is a test campaign";
   const TARGET = ethers.utils.parseEther("1");
-  const DURATION = 30 * 24 * 60 * 60; // 30 days
+  const DURATION = (30 * 24 * 60 * 60); // 30 days
   const IMAGE = "test_image";
   const OVERRIDE = { gasLimit: 2000000 };
 
@@ -41,8 +42,9 @@ describe("FundIt", function () {
     });
 
     it("Should deploy FundItProxy with the correct initial state", async function () {
-      expect(await fundItProxy.admin()).to.equal(fundItStorage.address);
-      expect(await fundItProxy.implementation()).to.equal(fundIt.address);
+      const proxyAdmin = new ethers.Contract(fundItStorage.address, ProxyAdmin.abi, owner);
+      expect(await proxyAdmin.getProxyAdmin(fundItProxy.address)).to.equal(fundItStorage.address);
+      expect(await proxyAdmin.getProxyImplementation(fundItProxy.address)).to.equal(fundIt.address);
     });
   });
 
@@ -71,134 +73,134 @@ describe("FundIt", function () {
     });
 
     it("Should revert when a non-owner tries to end a campaign", async function () {
-        await expect(fundIt.connect(addr1).endCampaign(0)).to.be.revertedWith("Only the campaign owner can end the campaign");
-      });
+      await expect(fundIt.connect(addr1).endCampaign(0)).to.be.revertedWith("Only the campaign owner can end the campaign");
+    });
       
-      it("Should revert when a non-owner tries to withdraw funds from a campaign", async function () {
-        // Fast-forward to the deadline
-        await ethers.provider.send("evm_increaseTime", [DURATION]);
-        await ethers.provider.send("evm_mine");
+    it("Should revert when a non-owner tries to withdraw funds from a campaign", async function () {
+      // Fast-forward to the deadline
+      await ethers.provider.send("evm_increaseTime", [DURATION]);
+      await ethers.provider.send("evm_mine");
       
-        await expect(fundIt.connect(addr1).withdrawFunds(0)).to.be.revertedWith("Only the campaign owner can withdraw funds");
-      });
+      await expect(fundIt.connect(addr1).withdrawFunds(0)).to.be.revertedWith("Only the campaign owner can withdraw funds");
+    });
+  });
+
+  // Test createCampaign function
+  describe("Create Campaign", function () {
+    it("Should create a new campaign", async function () {
+      const tx = await fundIt.createCampaign(TITLE, DESCRIPTION, TARGET, DURATION, IMAGE);
+      await tx.wait();
+      const campaign = await fundIt.campaigns(0);
+
+      expect(campaign.owner).to.equal(await ethers.provider.getSigner(0).getAddress());
+      expect(campaign.title).to.equal(TITLE);
+      expect(campaign.description).to.equal(DESCRIPTION);
+      expect(campaign.target).to.equal(TARGET);
+      expect(campaign.deadline).to.equal((await ethers.provider.getBlock("latest")).timestamp + DURATION);
+      expect(campaign.image).to.equal(IMAGE);
+      expect(campaign.active).to.equal(true);
+    });
+  });
+
+  // Test donateToCampaign function
+  describe("Donate to Campaign", function () {
+    beforeEach(async function () {
+      await fundIt.connect(owner).createCampaign(TITLE, DESCRIPTION, TARGET, DURATION, IMAGE, OVERRIDE);
     });
 
-    // Test createCampaign function
-    describe("Create Campaign", function () {
-        it("Should create a new campaign", async function () {
-        const tx = await fundIt.createCampaign(TITLE, DESCRIPTION, TARGET, DURATION, IMAGE);
-        await tx.wait();
-        const campaign = await fundIt.campaigns(0);
+    it("Should allow users to make donations to the campaign", async function () {
+      const donationAmount = ethers.utils.parseEther("0.1");
+      await fundIt.connect(addr1).donateToCampaign(0, { value: donationAmount });
+          
+      const campaign = await fundIt.campaigns(0);
+      expect(campaign.amountCollected).to.equal(donationAmount);
+          
+      const [donors, donations] = await fundIt.getCampaignDonors(0);
+      expect(donors[0]).to.equal(addr1.address);
+      expect(donations[0]).to.equal(donationAmount);
+    });
+  });
 
-        expect(campaign.owner).to.equal(await ethers.provider.getSigner(0).getAddress());
-        expect(campaign.title).to.equal(TITLE);
-        expect(campaign.description).to.equal(DESCRIPTION);
-        expect(campaign.target).to.equal(TARGET);
-        expect(campaign.deadline).to.equal((await ethers.provider.getBlock("latest")).timestamp + DURATION);
-        expect(campaign.image).to.equal(IMAGE);
-        expect(campaign.active).to.equal(true);
-        });
+  // Test withdrawFunds function
+  describe("Withdraw Funds", function () {
+    beforeEach(async function () {
+      await fundIt.connect(owner).createCampaign(TITLE, DESCRIPTION, TARGET, DURATION, IMAGE, OVERRIDE);
+      await fundIt.connect(addr1).donateToCampaign(0, { value: TARGET });
     });
 
-    // Test donateToCampaign function
-    describe("Donate to Campaign", function () {
-        beforeEach(async function () {
-            await fundIt.connect(owner).createCampaign(TITLE, DESCRIPTION, TARGET, DURATION, IMAGE, OVERRIDE);
-        });
-
-        it("Should allow users to make donations to the campaign", async function () {
-            const donationAmount = ethers.utils.parseEther("0.1");
-            await fundIt.connect(addr1).donateToCampaign(0, { value: donationAmount });
+    it("Should allow the campaign owner to withdraw funds after the deadline", async function () {
+      // Fast-forward to the deadline
+      await ethers.provider.send("evm_increaseTime", [DURATION]);
+      await ethers.provider.send("evm_mine");
           
-            const campaign = await fundIt.campaigns(0);
-            expect(campaign.amountCollected).to.equal(donationAmount);
+      const initialBalance = await owner.getBalance();
+      await fundIt.connect(owner).withdrawFunds(0);
           
-            const [donors, donations] = await fundIt.getCampaignDonors(0);
-            expect(donors[0]).to.equal(addr1.address);
-            expect(donations[0]).to.equal(donationAmount);
-        });
+      const campaign = await fundIt.campaigns(0);
+      expect(campaign.active).to.be.false;
+          
+      const finalBalance = await owner.getBalance();
+      expect(finalBalance).to.be.gt(initialBalance);
     });
+  });
 
-    // Test withdrawFunds function
-    describe("Withdraw Funds", function () {
-        beforeEach(async function () {
-            await fundIt.connect(owner).createCampaign(TITLE, DESCRIPTION, TARGET, DURATION, IMAGE, OVERRIDE);
-            await fundIt.connect(addr1).donateToCampaign(0, { value: TARGET });
-        });
-
-        it("Should allow the campaign owner to withdraw funds after the deadline", async function () {
-            // Fast-forward to the deadline
-            await ethers.provider.send("evm_increaseTime", [DURATION]);
-            await ethers.provider.send("evm_mine");
-          
-            const initialBalance = await owner.getBalance();
-            await fundIt.connect(owner).withdrawFunds(0);
-          
-            const campaign = await fundIt.campaigns(0);
-            expect(campaign.active).to.be.false;
-          
-            const finalBalance = await owner.getBalance();
-            expect(finalBalance).to.be.gt(initialBalance);
-        });
+  // Test endCampaign function
+  describe("End Campaign", function () {
+    beforeEach(async function () {
+      await fundIt.connect(owner).createCampaign(TITLE, DESCRIPTION, TARGET, DURATION, IMAGE, OVERRIDE);
     });
-
-    // Test endCampaign function
-    describe("End Campaign", function () {
-        beforeEach(async function () {
-            await fundIt.connect(owner).createCampaign(TITLE, DESCRIPTION, TARGET, DURATION, IMAGE, OVERRIDE);
-        });
         
-        it("Should allow the campaign owner to end the campaign if no funds were collected", async function () {
-            await fundIt.connect(owner).endCampaign(0);
-            const campaign = await fundIt.campaigns(0);
-            expect(campaign.active).to.be.false;
-        });
+    it("Should allow the campaign owner to end the campaign if no funds were collected", async function () {
+      await fundIt.connect(owner).endCampaign(0);
+      const campaign = await fundIt.campaigns(0);
+      expect(campaign.active).to.be.false;
+    });
+  });
+
+  // Test getActiveCampaigns function
+  describe("Get Active Campaigns", function () {
+    beforeEach(async function () {
+      await fundIt.connect(owner).createCampaign(TITLE, DESCRIPTION, TARGET, DURATION, IMAGE, OVERRIDE);
+      await fundIt.connect(addr1).createCampaign(TITLE, DESCRIPTION, TARGET, DURATION, IMAGE, OVERRIDE);
     });
 
-    // Test getActiveCampaigns function
-    describe("Get Active Campaigns", function () {
-        beforeEach(async function () {
-            await fundIt.connect(owner).createCampaign(TITLE, DESCRIPTION, TARGET, DURATION, IMAGE, OVERRIDE);
-            await fundIt.connect(addr1).createCampaign(TITLE, DESCRIPTION, TARGET, DURATION, IMAGE, OVERRIDE);
-        });
+    it("Should return all active campaigns", async function () {
+      const activeCampaigns = await fundIt.getActiveCampaigns();
+      expect(activeCampaigns.length).to.equal(2);
+    });
+  });
 
-        it("Should return all active campaigns", async function () {
-            const activeCampaigns = await fundIt.getActiveCampaigns();
-            expect(activeCampaigns.length).to.equal(2);
-        });
+  // Test getEndedCampaigns function
+  describe("Get Ended Campaigns", function () {
+    beforeEach(async function () {
+      await fundIt.connect(owner).createCampaign(TITLE, DESCRIPTION, TARGET, DURATION, IMAGE, OVERRIDE);
+      await fundIt.connect(addr1).createCampaign(TITLE, DESCRIPTION, TARGET, DURATION, IMAGE, OVERRIDE);
+      // Fast-forward to the deadline
+      await ethers.provider.send("evm_increaseTime", [DURATION]);
+      await ethers.provider.send("evm_mine");
     });
 
-    // Test getEndedCampaigns function
-    describe("Get Ended Campaigns", function () {
-        beforeEach(async function () {
-            await fundIt.connect(owner).createCampaign(TITLE, DESCRIPTION, TARGET, DURATION, IMAGE, OVERRIDE);
-            await fundIt.connect(addr1).createCampaign(TITLE, DESCRIPTION, TARGET, DURATION, IMAGE, OVERRIDE);
-            // Fast-forward to the deadline
-            await ethers.provider.send("evm_increaseTime", [DURATION]);
-            await ethers.provider.send("evm_mine");
-        });
+    it("Should return all ended campaigns", async function () {
+      const endedCampaigns = await fundIt.getEndedCampaigns();
+      expect(endedCampaigns.length).to.equal(2);
+    });
+  });
 
-        it("Should return all ended campaigns", async function () {
-            const endedCampaigns = await fundIt.getEndedCampaigns();
-            expect(endedCampaigns.length).to.equal(2);
-        });
+  // Test getCampaignDonors function
+  describe("Get Campaign Donors", function () {
+    beforeEach(async function () {
+      await fundIt.connect(owner).createCampaign(TITLE, DESCRIPTION, TARGET, DURATION, IMAGE, OVERRIDE);
+      await fundIt.connect(addr1).donateToCampaign(0, { value: ethers.utils.parseEther("0.1") });
+      await fundIt.connect(addr2).donateToCampaign(0, { value: ethers.utils.parseEther("0.2") });
     });
 
-    // Test getCampaignDonors function
-    describe("Get Campaign Donors", function () {
-        beforeEach(async function () {
-            await fundIt.connect(owner).createCampaign(TITLE, DESCRIPTION, TARGET, DURATION, IMAGE, OVERRIDE);
-            await fundIt.connect(addr1).donateToCampaign(0, { value: ethers.utils.parseEther("0.1") });
-            await fundIt.connect(addr2).donateToCampaign(0, { value: ethers.utils.parseEther("0.2") });
-        });
-
-        it("Should return all donors and their donations for a campaign", async function () {
-            const [donors, donations] = await fundIt.getCampaignDonors(0);
-            expect(donors.length).to.equal(2);
-            expect(donors[0]).to.equal(addr1.address);
-            expect(donors[1]).to.equal(addr2.address);
-            expect(donations[0]).to.equal(ethers.utils.parseEther("0.1"));
-            expect(donations[1]).to.equal(ethers.utils.parseEther("0.2"));
-        });
+    it("Should return all donors and their donations for a campaign", async function () {
+      const [donors, donations] = await fundIt.getCampaignDonors(0);
+      expect(donors.length).to.equal(2);
+      expect(donors[0]).to.equal(addr1.address);
+      expect(donors[1]).to.equal(addr2.address);
+      expect(donations[0]).to.equal(ethers.utils.parseEther("0.1"));
+      expect(donations[1]).to.equal(ethers.utils.parseEther("0.2"));
     });
+  });
 });
